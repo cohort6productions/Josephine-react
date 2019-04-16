b4b86bf0698671f3d95a7fffd4284f815c2a2ae5
ivanoung <ivanoung@gmail.com>
2019-04-16 15:53:04 +0800

    added all service pages

--- src/routes/defaultRoutes.tsx
+++ src/routes/defaultRoutes.tsx
@@ -13,12 +13,20 @@ import { nameToPathConvert } from "src/helper/helper";
 // General
 import Homepage from "src/pages/1-homepage/homepage";
 import Error404 from "src/pages/error404/error404";
+
 // How to start
 import OpeningNewBusiness from "src/pages/2a-openingNewBusiness/openingNewBusiness";
+import MaintainingUpkeeping from "src/pages/2b-maintainingUpkeeping/maintainingUpkeeping";
+import ExpandingBusiness from "src/pages/2c-expandingBusiness/expandingBusiness";
+import VisaLicencing from "src/pages/2d-visaLicencing/visaLicencing";
+import BusinessEnteringChina from "src/pages/2e-businessEnteringChina/businessEnteringChina";
+import BusinessLeavingHongKong from "src/pages/2f-businessLeavingHongKong/businessLeavingHongKong";
+
 // Often use
 import CompanyIncorporation from "src/pages/3a-companyIncorporation/companyIncorporation";
 import IncorporationProcess from "src/pages/3a Incorporation/3a";
 import ConferenceRoomBooking from "src/pages/3d-conferenceRoomBooking/conferenceRoomBooking";
+
 // Resources
 import Newsletter from "src/pages/5b-newsletter/newsletter";
 import AboutUs from "src/pages/5c-aboutUs/aboutUs";
@@ -44,11 +52,27 @@ class DefaultRoutes extends React.Component<IDefaultRoutesProps> {
                     path="/open"
                     component={OpeningNewBusiness}
                 />
-                <Route exact={true} path="/upkeep" component={Homepage} />
-                <Route exact={true} path="/expand" component={Homepage} />
-                <Route exact={true} path="/china" component={Homepage} />
-                <Route exact={true} path="/visa" component={Homepage} />
-                <Route exact={true} path="/relocate" component={Homepage} />
+                <Route
+                    exact={true}
+                    path="/upkeep"
+                    component={MaintainingUpkeeping}
+                />
+                <Route
+                    exact={true}
+                    path="/expand"
+                    component={ExpandingBusiness}
+                />
+                <Route
+                    exact={true}
+                    path="/china"
+                    component={BusinessEnteringChina}
+                />
+                <Route exact={true} path="/visa" component={VisaLicencing} />
+                <Route
+                    exact={true}
+                    path="/relocate"
+                    component={BusinessLeavingHongKong}
+                />
                 {/* End of "How to start" */}
 
                 {/* Start of "Often use" */}
